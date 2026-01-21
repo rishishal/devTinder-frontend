80bb2d738aacb392082a77055add4405847309b0
rishiVaraha <rishi.kumar@varahaworld.com>
2026-01-21 15:36:53 +0530

    add Logo and Svg

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,32 +1,24 @@
 import { Link } from "@tanstack/react-router";
 import { useAuthStore } from "@/stores/auth-store";
+import LOGO from "@/assets/logo.svg?react";
 
 export const Navbar = () => {
   const { auth } = useAuthStore();
 
   return (
     <nav className="p-4 flex gap-4 bg-gray-100 items-center">
-
-      {/* Show profile link only if authenticated */}
-      {auth.isAuthenticated && (
-        <Link to="/profile" className="[&.active]:font-bold">
-          Profile
-        </Link>
-      )}
-
-      {/* Spacer */}
+      <div className="flex items-center gap-2.5">
+        <LOGO className="w-10 h-10" />
+        <p className="text-lg font-bold">Code & Connect</p>
+      </div>
       <div className="flex-1" />
 
-      {/* Auth status indicator */}
       {auth.isAuthenticated ? (
         <span className="text-sm text-gray-600">
           Hello, {auth.user?.firstName}!
         </span>
       ) : (
-        <Link
-          to="/sign-in"
-          className="text-sm text-blue-600 hover:underline"
-        >
+        <Link to="/sign-in" className="text-sm ">
           Sign In
         </Link>
       )}
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-
+import svgr from 'vite-plugin-svgr'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite(), tailwindcss()],
+  plugins: [tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }), react(), tailwindcss(), svgr()],
+    
+    optimizeDeps: {
+    include: ['axios'],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
